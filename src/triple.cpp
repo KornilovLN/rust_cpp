b30413088733999ac9bf4cf17a7fd97735e65576
KornilovLN <ln.KornilovStar@gmail.com>
2023-09-18 17:35:49 +0300

    Del <file>.rs

--- src/triple.cpp
+++
@@ -1,8 +0,0 @@
-//--- Эта функция отработает при вызове из main.rs
-//--- В файле main.rs должна быть объявлена функция с подобной сигнатурой,
-//--- т.е. fn triple_input(input: libc::c_int) -> libc::c_int;
- 
-extern "C"
-int triple_input(int input) {
-    return input * 3;
-}
